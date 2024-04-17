e855bf4574347a00d275fd6f83a10cfa599c5f49
Serin Serena Jeon <serin0314@gmail.com>
2024-04-17 18:24:24 -0400

    removed lab material

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import Labs from "./Labs";
-import HelloWorld from "./Labs/a3/HelloWorld";
 import Kanbas from "./Kanbas";
 import {HashRouter} from "react-router-dom"
 import {Routes, Route, Navigate} from "react-router";
@@ -9,10 +7,8 @@ function App() {
     <HashRouter>
       <div>
         <Routes>
-          <Route path="/"         element={<Navigate to="/Labs"/>}/>
-          <Route path="/Labs/*"   element={<Labs/>}/>
+          <Route path="/"         element={<Navigate to="/Kanbas/Dashboard"/>}/>
           <Route path="/Kanbas/*" element={<Kanbas/>}/>
-          <Route path="/hello"    element={<HelloWorld/>}/>
         </Routes>
       </div>
     </HashRouter>
