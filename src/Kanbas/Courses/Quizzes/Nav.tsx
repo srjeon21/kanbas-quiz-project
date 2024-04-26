2bb8bfdf31dee185b6fc349a7204acb43b726645
Serin Serena Jeon <serin0314@gmail.com>
2024-04-26 23:30:12 +0900

    nav url fix

--- src/Kanbas/Courses/Quizzes/Nav.tsx
+++ src/Kanbas/Courses/Quizzes/Nav.tsx
@@ -2,12 +2,17 @@ import {Link, useLocation } from "react-router-dom";
 
 function Nav() {
     const { pathname } = useLocation();
+    const removeEnd = () => {
+        const path = pathname.split('/');
+        const last = path.pop();
+        return path.join('/');
+    };
     return (
         <nav className="nav nav-tabs mt-2">
             <Link className={`nav-link ${pathname.includes("details") ? "active" : ""}`}
-                to="./details">Details</Link>
+                to={`${removeEnd()}/details`}>Details</Link>
             <Link className={`nav-link ${pathname.includes("questions") ? "active" : ""}`}
-                to="./questions">Questions</Link>
+                to={`${removeEnd()}/questions`}>Questions</Link>
         </nav>
     );
 } export default Nav;
